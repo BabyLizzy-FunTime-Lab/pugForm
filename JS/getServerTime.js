57ea23f262240c974c7bbb6befeaeac40ecefa31
José Rasmussen <j.l.rasmussengarcia@gmail.com>
2017-09-13 13:39:25 +0200

    Trying to get fecth to return the object serverside

--- JS/getServerTime.js
+++ JS/getServerTime.js
@@ -19,3 +19,41 @@ function updateClock() {
 
 setInterval(updateClock, 3000);
 
+
+
+function sup(event) {
+	event.preventDefault();
+	let formElement = document.getElementById('greetingForm');
+	console.log(formElement);
+	let form = new FormData (formElement);
+	// let bar = new FormData();
+	// bar.append("daskj", "xxxx");
+	// bar.append("foo", document.getElementById("greetingInput").value);
+	let serverGreet = document.getElementById("sup");
+	console.log("send");
+	// console.log(bar);
+
+	fetch('/sup', {
+		method: "POST",
+		body: form,
+		headers: new Headers({
+			// "Content-Type": "multipart/form-data"
+			"Content-Type": "application/x-www-form-urlencoded"
+		})
+	})
+	.then(function (response) {
+		response.text().then(function(text) {
+			serverGreet.innerHTML = text;
+			console.log(text)
+		})
+	})
+	.catch( 
+		function (error) {
+		console.log("derp on sup")
+		}
+	);
+};
+
+document.addEventListener("DOMContentLoaded", function() {
+	document.getElementById("submitBtn").addEventListener("click", sup);
+});
--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 const express = require('express');
 // const pug = require('pug');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
 
 const server = express();
 
+//Makig users for practice
+let users = [
+	{
+		"name" : " ",
+		"password": " ",
+		"e-mail": " "
+	},
+	{
+		"name" : " ",
+		"password": " ",
+		"e-mail": " "	
+	}
+];
+
+//Load hashed passwords
+
+
 server.use(bodyParser.urlencoded({extended: true}));
+// this works when the data send is plain text. In the fetch body
+// plain text.
+// In the fetch header you need
+// "content-type": "text/plain"
+server.use(bodyParser.text());
 server.set("view engine", "pug");
 
 server.use('/public', express.static(__dirname + '/public'));
@@ -25,6 +48,12 @@ function serverTime() {
 server.get('/serverTime', function (req, res) {
 	res.send(serverTime())
 });
+
+server.post('/sup', function (req, res) {
+	console.log("getting stuff sdfsdfds");
+	console.log("req.body = ", req.body);
+	res.send("back at you");
+})
 
 server.listen(3000, function() {
 	console.log("Server running on port 3000");
