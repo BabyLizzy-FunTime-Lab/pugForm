ca02dbd7fbd604881bbf7c9d784305c896f83d4d
José Rasmussen <j.l.rasmussengarcia@gmail.com>
2017-09-14 13:11:15 +0200

    still working on fetch form object

--- JS/getServerTime.js
+++ JS/getServerTime.js
@@ -24,26 +24,27 @@ setInterval(updateClock, 3000);
 function sup(event) {
 	event.preventDefault();
 	let formElement = document.getElementById('greetingForm');
-	console.log(formElement);
+	console.log("This is the form" + formElement);
 	let form = new FormData (formElement);
 	// let bar = new FormData();
 	// bar.append("daskj", "xxxx");
 	// bar.append("foo", document.getElementById("greetingInput").value);
 	let serverGreet = document.getElementById("sup");
 	console.log("send");
-	// console.log(bar);
+	console.log(form);
 
 	fetch('/sup', {
 		method: "POST",
-		body: form,
 		headers: new Headers({
-			// "Content-Type": "multipart/form-data"
+			"Accept": "application/json, application/xml, text/plain, text/html",
 			"Content-Type": "application/x-www-form-urlencoded"
-		})
+			// "Content-Type": "multipart/form-data"
+		}),
+		body: form
 	})
 	.then(function (response) {
 		response.text().then(function(text) {
-			serverGreet.innerHTML = text;
+			serverGreet.innerHTML += text;
 			console.log(text)
 		})
 	})
@@ -55,5 +56,7 @@ function sup(event) {
 };
 
 document.addEventListener("DOMContentLoaded", function() {
+	// This way you don't need an onclick in the button element. 
+	// You run sup() on click.
 	document.getElementById("submitBtn").addEventListener("click", sup);
 });
